84bad75faeed975133485e502734fd1837d0b226
observer <ozzyundead@gmail.com>
2023-11-08 11:19:20 +0200

    adding movement to left side

--- ship.py
+++ ship.py
@@ -3,10 +3,10 @@ import pygame
 
 class Ship():
 
-    def __init__(self, ai_games):
+    def __init__(self, ai_game):
         """Initialization of the ship and his start position """
-        self.screen = ai_games.screen
-        self.screen_rect = ai_games.screen.get_rect()
+        self.screen = ai_game.screen
+        self.screen_rect = ai_game.screen.get_rect()
 
         # Download image and his rect
         self.image = pygame.image.load('alien_invasion\images\ship.bmp')
@@ -14,6 +14,17 @@ class Ship():
 
         # Create every new ship in bottom of the screen
         self.rect.midbottom = self.screen_rect.midbottom
+
+        # Movement indicators
+        self.moving_right = False
+        self.moving_left = False
+
+    def update(self):
+        # Update position of the ship base on movemet indicators
+        if self.moving_right:
+            self.rect.x += 1
+        if self.moving_left:
+            self.rect.x -= 1
 
     def blitme(self):
         self.screen.blit(self.image, self.rect)
